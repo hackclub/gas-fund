aea2a80835d01676e130e3b78552f94e8a411b64
Ivoine <ivoinestrachan242@gmail.com>
2023-11-21 15:58:47 -0500

    hide easter egg then show when hover

--- components/EasterEgg.tsx
+++ components/EasterEgg.tsx
@@ -1,8 +1,10 @@
 export const EasterEgg = () => {
   return (
-    <div className="overflow-hidden">
-      <img src="/gasicon.svg" className="w-36 translate-y-12 rotate-12" />
-      {/* <img src="/gasicon.svg" className="w-36 opacity-0" /> */}
+    <div className="group overflow-hidden">
+      <img 
+        src="/gasicon.svg" 
+        className="w-36 translate-y-12 rotate-12 group-hover:opacity-100 opacity-0 transition-opacity duration-300 ease-in-out"
+      />
     </div>
   );
 };
