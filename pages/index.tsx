0483760855521df98b4719a7e843e4ca03dd9433
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-09-26 18:58:34 -0400

    Add title to gas fund page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,27 +8,33 @@ import { TrainTrack } from "@/components/TrainTrack";
 import { TrainsAndBuses } from "@/components/TrainsAndBuses";
 import Works from "@/components/Works";
 import { motion, useScroll } from "framer-motion";
+import Head from "next/head";
 
 export default function Home() {
   const { scrollY } = useScroll();
   return (
-    <div className="overflow-x-hidden">
-      <Nav />
-      <Hero />
-      <Explanation />
+    <>
+      <Head>
+        <title>Gas Fund</title>
+      </Head>
+      <div className="overflow-x-hidden">
+        <Nav />
+        <Hero />
+        <Explanation />
 
-      <img src="/roadbanner.svg" className="w-full my-[5vh]" />
-      <Works />
-      <div className="mt-2">
-        <TrainTrack />
-      </div>
-      <TrainsAndBuses />
+        <img src="/roadbanner.svg" className="w-full my-[5vh]" />
+        <Works />
+        <div className="mt-2">
+          <TrainTrack />
+        </div>
+        <TrainsAndBuses />
 
-      <div className="border-t-[3px] border-gray-100 mt-8 pt-4 border-dotted">
-        <GetStarted />
+        <div className="border-t-[3px] border-gray-100 mt-8 pt-4 border-dotted">
+          <GetStarted />
+        </div>
+        <Footer />
+        <EasterEgg />
       </div>
-<Footer />
-      <EasterEgg />
-    </div>
+    </>
   );
 }
